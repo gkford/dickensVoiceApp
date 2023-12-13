f5e30f2676ef8a4f20959067591fc60939cd0d4a
gkford <gkford@gmail.com>
2023-12-14 12:46:06 +1300

    (no commit message provided)

---
+++ llm.ts
@@ -0,0 +1,19 @@
+
+
+{
+    "id": "asst_outline_helper",
+    "object": "assistant",
+    "created_at": 1698984975,
+    "name": "Outline Helper",
+    "description": "A specialized assistant for managing and updating outlines for articles and projects, optimized for voice mode.",
+    "model": "gpt-4",
+    "instructions": "Outline Helper is designed to manage and update outlines for articles and projects. It reads and writes outlines stored in HTML format, and provides suggestions for structuring and organizing content.",
+    "tools": [
+      {
+        "type": "html_editor"
+      }
+    ],
+    "file_ids": [],
+    "metadata": {}
+  }
+  
